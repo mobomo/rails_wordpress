757f4a5900ac52db1f73060fb821a38568765f2d
harikrishnantb <harikrishnan@qburst.com>
2016-12-05 14:09:34 +0530

    rails blog

--- app/models/wordpress/wordpress_database.rb
+++ app/models/wordpress/wordpress_database.rb
@@ -1,6 +1,6 @@
 module Wordpress
   class WordpressDatabase < ActiveRecord::Base
     self.abstract_class = true
-    establish_connection ENV.fetch("WORDPRESS_DATABASE", 'some_wordpress_database').to_sym
+    establish_connection ENV.fetch("WORDPRESS_DATABASE", 'rails_blog').to_sym
   end
 end
